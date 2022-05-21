7aa7da8f820bceca0fe12aff7ad439fd45d0370f
Rohit Singh Bisht <rohitsb485@gmail.com>
2022-05-21 14:28:55 +0530

    Increased bottom margin for logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
   return (
     <div className="w-full flex flex-col sm:flex-row justify-center sm:justify-between items-center fixed top-0 p-3 sm:p-5 bg-white border-b-2 border-accent">
       <Link to="/">
-        <div className="text-2xl font-bold mb-3 sm:mb-0">Game Explorer</div>
+        <div className="text-2xl font-bold mb-4 sm:mb-0">Game Explorer</div>
       </Link>
       <div className="flex justify-evenly sm:justify-end sm:space-x-8 w-full sm:w-0">
         <Link to="/popular-games">
