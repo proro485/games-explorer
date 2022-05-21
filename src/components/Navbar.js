af28e130b5f635fed5fd268d3d3e158d179161ab
Rohit Singh Bisht <rohitsb485@gmail.com>
2022-05-21 15:03:08 +0530

    Made navbar modular and highlighted the link which we are on with different color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
@@ -7,23 +7,33 @@ export const Navbar = () => {
       <Link to="/">
         <div className="text-2xl font-bold mb-4 sm:mb-0">Game Explorer</div>
       </Link>
-      <div className="flex justify-evenly sm:justify-end sm:space-x-8 w-full sm:w-0">
-        <Link to="/popular-games">
-          <div className="font-bold text-gray-500 hover:text-green-900">
-            Popular
-          </div>
-        </Link>
-        <Link to="/upcoming-games">
-          <div className="font-bold text-gray-500 hover:text-green-900">
-            Upcoming
-          </div>
-        </Link>
-        <Link to="/new-games">
-          <div className="font-bold text-gray-500 hover:text-green-900">
-            New
-          </div>
-        </Link>
-      </div>
+      <NavbarLinks />
     </div>
+  );
+};
+
+const NavbarLinks = () => {
+  return (
+    <div className="flex justify-evenly sm:justify-end sm:space-x-8 w-full sm:w-fit">
+      <NavbarLink path="popular-games" text="Popular" />
+      <NavbarLink path="upcoming-games" text="Upcoming" />
+      <NavbarLink path="new-games" text="New" />
+    </div>
+  );
+};
+
+const NavbarLink = ({ path, text }) => {
+  const location = useLocation().pathname.split('/')[1];
+
+  return (
+    <Link to={`/${path}`}>
+      <div
+        className={`font-bold ${
+          location === path ? 'text-green-900' : 'text-gray-500 '
+        } hover:text-green-900`}
+      >
+        {text}
+      </div>
+    </Link>
   );
 };
